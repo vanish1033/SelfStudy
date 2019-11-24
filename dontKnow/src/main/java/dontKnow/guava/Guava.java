46448a8a30effc1d92d9cfa3ced6596e8a6c148f
whr <1033159085@qq.com>
2019-11-24 10:42:13 +0800

    Guava1.java

--- dontKnow/src/main/java/dontKnow/guava/Guava.java
+++ dontKnow/src/main/java/dontKnow/guava/Guava.java
@@ -37,10 +37,8 @@ public class Guava {
         strings.add("1");
         strings.add("2");
         strings.add("3");
-
-        strings.add("5");
-
         strings.add("4");
+        strings.add("5");
 
 
 //        List<Integer> integers1 = Collections.unmodifiableList(integers);
