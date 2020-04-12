525c8c30b1c716b13673be637adb57e95b085be0
wanghongrui <1033159085@qq.com>
2020-04-12 16:17:14 +0800

    "更新了Test练习文件"

--- dontKnow/src/main/java/dontKnow/Test.java
+++ dontKnow/src/main/java/dontKnow/Test.java
@@ -1,13 +1,11 @@
 package dontKnow;
 
 import com.alibaba.fastjson.JSON;
-import com.google.common.collect.Lists;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import myannotation.ProblemCode;
 import org.apache.commons.lang3.StringUtils;
@@ -25,7 +23,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
-import java.net.SocketAddress;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.text.DecimalFormat;
@@ -36,6 +33,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -909,69 +907,135 @@ public class Test {
      */
     @org.junit.Test
     public void test48() {
-        ArrayList<Integer> integers = Lists.newArrayList(1, 2, 3, 4);
-        ArrayList<Object> objects = Lists.newArrayList();
-        integers.stream().forEach((x) -> objects.add(x));
-        System.out.println(objects);
-        Collections.reverse(objects);
-//        objects.
-        Integer[] parentIds = new Integer[]{1, 2, 3, 4, 5};
-        ArrayList<Integer> parentIdsReverse = new ArrayList<>();
-        Collections.addAll(parentIdsReverse, parentIds);
-        Collections.reverse(parentIdsReverse);
-        String join = StringUtils.join(parentIdsReverse, "/");
-        System.out.println(join);
 
-        System.out.println(Stream.iterate((int) 2, x -> x * x).mapToDouble(Double::valueOf).limit(67).max());
-        log.info("Arrays.stream(parentIds).reduce((x, y) -> x >= y ? x : y).orElse(-1) = " + Arrays.stream(parentIds).reduce((x, y) -> x >= y ? x : y).orElse(-1));
+        List<String> list = new ArrayList<>();
+        Collections.addAll(list, "wkcto", "good", "hello", "jj", "xx", "jj", "jj", "bjpowernode", "xx", "good");
+        //1)通过List集合获得Stream流
+        Stream<String> stream = list.stream();
 
-//        @PostMapping({"add"})
-//        @Log(
-//                logType = LogType.INSERT
-//        )
-////@RequiresMethodPermissions({"add"})
-//        public Response add(DicLable entity, BindingResult result) {
-//            this.checkError(entity, result);
-//            if (entity != null) {
-//                entity.setId(UUIDUtils.getUUID());
-//                User user = UserUtils.getUser();
-//                entity.setCreateBy(user.getId());
-//                entity.setCreateDate(new Date());
-//                StringBuffer parentIds = new StringBuffer("");
-//                if (StringUtils.isNotEmpty(entity.getParentId())) {
-//                    parentIds = getParentIds(parentIds, entity.getParentId());
-//                    String[] split = parentIds.toString().split("/");
-//                    List<String> arrayList = new ArrayList<String>();
-//                    int length = split.length;
-//                    for (int i = 0; i < length; i++) {
-//                        arrayList.add(split[length - i - 1]);
-//                        arrayList.add("/");
-//                    }
-//                    String str = "";
-//                    for (String s : arrayList) {
-//                        str += s;
-//                    }
-//                    if (str.endsWith("/")) {
-//                        str.substring(0, str.length() - 1);
-//                    }
-//                    System.out.println("parentIds = " + str);
-//                    entity.setParentIds(str);
-//                }
-//                dicLableService.insertByEntity(entity);
-//                return Response.ok("添加成功");
-//            }
-//            return Response.error("添加失败");
-//        }
-//
-//        private StringBuffer getParentIds(StringBuffer parentIds, String parentId) {
-//            StringBuffer append = parentIds.append(parentId).append("/");
-//            DicLable dicLable = dicLableService.selectParentId(parentId);
-//            if (StringUtils.isNotEmpty(dicLable.getParentId())) {
-//                StringBuffer parentIds1 = getParentIds(append, dicLable.getParentId());
-//                return parentIds1;
-//            }
-//            return append;
-//        }
+        //2流的中间操作
+        //2.1 流的筛选与切片
+        //distinct()去掉重复的数据
+        stream.distinct().forEach(System.out::println);
+        System.out.println("-----------------------");
+
+        //filter()过滤
+//        stream.filter(x->x.length() > 4).forEach(System.out::println);  //java.lang.IllegalStateException: 流已关闭, 流只能使用一次
+        list.stream().filter(x -> x.length() > 4).forEach(System.out::println);
+        System.out.println("-----------------------");
+
+        //sort()排序
+        list.stream().sorted(String::compareTo).forEach(System.out::println);
+        System.out.println("-----------------------");
+
+        //limt()截断操作
+        list.stream().limit(5).forEach(System.out::println);
+        System.out.println("-----------------------");
+
+        //skip()跳过
+        list.stream().skip(3).forEach(System.out::println);
+        System.out.println("-----------------------");
+
+        //2.2 map映射
+        //map映射转换元素, map方法接受一个函数, 把这个函数应用于每个元素上,并将它映射为一个新的元素
+        list.stream()
+                .map(String::toUpperCase)   //为每个元素应用toUpperCase()把小写转换为大写
+                .forEach(System.out::println);
+        System.out.println("-----------------------");
+
+        list.stream()
+                .map(String::length)
+                .map(len -> len + "  ")
+                .forEach(System.out::print);
+        System.out.println();
+        System.out.println("-----------------------");
+
+        //转换为数值流
+        List<Integer> integerList = Arrays.asList(54, 1, 78, 90, 345);
+        IntStream intStream = integerList.stream().mapToInt(x -> x);
+        intStream.forEach(x -> System.out.print(x + " "));
+        System.out.println();
+        System.out.println("-----------------------");
+
+        //2.3 匹配与查找操作
+        //allMatch()判断流中所有的元素是否都匹配给定的谓词
+        System.out.println(list.stream().allMatch(s -> s.length() > 3));     //false
+        //anyMatch()判断流中是否有某个元素可以匹配指定的谓词
+        System.out.println(list.stream().anyMatch(s -> s.equals("wkcto")));    //true
+        //noneMathc()判断流中的元素是否都没有匹配指定谓词的
+        System.out.println(list.stream().noneMatch(s -> s.equals("jj")));      //false
+
+        //查找
+        System.out.println(list.stream().filter(s -> s.length() > 10).findAny().get());
+        System.out.println(list.stream().filter(s -> s.length() > 20).findFirst().orElse("不存在"));
+
+        //3 Stream流的终端操作
+        //3.1 forEach遍历
+        list.stream().forEach(System.out::println);
+
+        //3.2 cout统计
+        System.out.println(list.stream().filter(x -> x.length() > 2).count());
+
+        //3.3 reduce归纳合并
+        Optional<String> reduce = list.stream().reduce((s1, s2) -> s1 + "--" + s2);
+
+        /**
+         * https://segmentfault.com/q/1010000004944450 reduce三个参数方法的解释
+         */
+        String[] reduce1 = list.stream().reduce(new String[100], (x, y) -> {
+            ArrayList<String> strings1 = new ArrayList<>();
+            Collections.addAll(strings1, x);
+            strings1.add(y);
+            return strings1.toArray(new String[100]);
+        }, (x, y) -> {
+            ArrayList<String> c = new ArrayList<>();
+            Collections.addAll(c, x);
+            Collections.addAll(c, y);
+            return c.toArray(new String[100]);
+        });
+
+
+        System.out.println(reduce1);
+        reduce.ifPresent(System.out::println);
+
+        //数值操作
+        List<Integer> list2 = Arrays.asList(6, 21, 87, 34, 1, 78, 54);
+        //求和,指定初始值
+        System.out.println(list2.stream().reduce(0, Integer::sum));
+        //求和,没有初始值
+        System.out.println(list2.stream().reduce((x, y) -> x + y).orElse(0));
+        //最值
+        System.out.println(list2.stream().reduce(Math::max).get());
+        System.out.println(list2.stream().reduce(Math::min).get());
+
+        //3.4 映射到数值流
+        System.out.println(list2.stream().mapToInt(x -> x).sum());       //求和
+        System.out.println(list2.stream().mapToInt(x -> x).max().getAsInt());    //最大值
+        System.out.println(list2.stream().mapToInt(x -> x).min().orElse(0));     //最小值
+        System.out.println(list2.stream().mapToDouble(x -> x).average().getAsDouble());   //平均值
+        System.out.println(list2.stream().max(Integer::compareTo).get());
+        list2.stream().min(Integer::compareTo).ifPresent(System.out::println);
+
+        /**
+         * 3.5 collect归约
+         * https://www.jianshu.com/p/ccbb42ad9551 -> Stream类的collect方法详解
+         */
+        //把stream流转换为集合
+        Set<String> stringSet = list.stream().collect(Collectors.toSet());
+        System.out.println(stringSet);
+
+        //把Stream流转换为数组
+        Object[] objects = list.stream().toArray();
+        System.out.println(Arrays.toString(objects));
+        String[] toArray = list.stream().toArray(String[]::new);
+        System.out.println(Arrays.toString(toArray));
+
+        //Stream流转换为字符串
+        String collect = list.stream().collect(Collectors.joining(","));
+        List<String> collect1 = list.stream().collect(Collectors.toList());
+        System.out.println(collect);
+
+
     }
 
 }
