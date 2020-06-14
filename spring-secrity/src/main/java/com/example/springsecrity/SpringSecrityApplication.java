5658198a4fff4e71972147962c3dfa7530d25579
whr <1033159085@qq.com>
2020-06-14 15:28:42 +0800

    spring-secrity

---
+++ spring-secrity/src/main/java/com/example/springsecrity/SpringSecrityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecrity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecrityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecrityApplication.class, args);
+    }
+
+}
