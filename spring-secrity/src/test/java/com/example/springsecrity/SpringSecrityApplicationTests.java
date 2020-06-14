892b4f951f2492f34029558cc734e8637bcc052d
whr <1033159085@qq.com>
2020-06-14 16:47:01 +0800

    spring-secrity

---
+++ spring-secrity/src/test/java/com/example/springsecrity/SpringSecrityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecrity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecrityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
